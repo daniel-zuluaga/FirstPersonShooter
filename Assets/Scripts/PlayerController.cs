f8d3eb42e40fa5a86cccc25b4fb06563bbdc50a2
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-09-05 17:10:42 -0500

    In this commit I add the movement of the player, the camera and the jump to be able to dodge the obstacles

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float jumpForce;
+
+    [Header("Camera")]
+    public float lookSensibility;
+    public float maxLookX;
+    public float minLookX;
+    private float rotX;
+
+    private Camera cam;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        // get components
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+
+        if(Input.GetButtonDown("Jump"))
+            TryJump();
+
+        CamLook();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal")* moveSpeed;
+        float z = Input.GetAxis("Vertical")* moveSpeed;
+
+        Vector3 dir = transform.right * x + transform.forward * z;
+        dir.y = rb.velocity.y;
+
+        rb.velocity = dir;
+    }
+
+    void CamLook()
+    {
+        float y = Input.GetAxis("Mouse X") * lookSensibility;
+        rotX += Input.GetAxis("Mouse Y") * lookSensibility;
+
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+
+        cam.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;
+
+    }
+
+    void TryJump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, 1.1f))
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse );
+    }
+
+}
