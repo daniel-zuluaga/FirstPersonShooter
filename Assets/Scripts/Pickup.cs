868ef8c35bd5b3529b98f0923b106a04e41328d8
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-09-17 11:35:19 -0500

    I added an animation that goes to the pickup so that it is not something crappy

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -13,5 +13,54 @@ public class Pickup : MonoBehaviour
     public PickupType type;
     public int value;
 
+    [Header("Bodding")]
+    public float rotateSpeed;
+    public float bobSpeed;
+    public float bobHeight;
+
+    private Vector3 startPos;
+    private bool bobbingUp;
+
+    private void Start()
+    {
+        // set the start position
+        startPos = transform.position;
+        
+    }
+
+    private void Update()
+    {
+        // rotating
+        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+
+        // bob up and down
+        Vector3 offset = (bobbingUp == true ? new Vector3(0, bobHeight / 2, 0) : new Vector3(0, -bobHeight / 2, 0));
+        transform.position = Vector3.MoveTowards(transform.position, startPos + offset, bobSpeed * Time.deltaTime);
+
+        if(transform.position == startPos + offset)
+            bobbingUp = !bobbingUp;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+
+            switch (type)
+            {
+                case PickupType.Health:
+                    player.GiveHealth(value);
+                    break;
+                case PickupType.Ammo:
+                    player.GiveAmmo(value);
+                    break;
+            }
+
+            Destroy(gameObject);
+
+        }
+    }
 
 }
