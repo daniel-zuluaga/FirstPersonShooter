980a9d4c3d18623980fa7cc0089a8453dc4bb315
Daniel Esteban Zuluaga Salazar <70453255+daniel-zuluaga@users.noreply.github.com>
2022-09-24 21:51:44 -0500

    feature/GameUI
    Well add many things in my shooter like:
    - the player's life bar, the score and the bullets he will have
    - the pause screen and the button to pause
    - win or lose screen
    - the main menu screen
    but it needs to be coded to make it work

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour
+{
+
+    [Header("HUD")]
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI ammoText;
+    public Image healthBarFill;
+
+    [Header("Pause Menu")]
+    public GameObject pauseMenu;
+    public GameObject pauseButton;
+
+    [Header("End Game Screen")]
+    public GameObject endGameScreen;
+    public TextMeshProUGUI endGameHeaderText;
+    public TextMeshProUGUI endGameScoreText;
+
+    // instance
+    public static GameUI instance;
+
+    private void Awake()
+    {
+        // set the instance to this script
+        instance = this;
+
+    }
+
+    public void UpdateHealthBar (int curHp, int maxHp)
+    {
+        healthBarFill.fillAmount = (float)curHp / (float)maxHp;
+    }
+
+    public void UpdateScoreText (int score)
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void UpdateAmmoText (int curAmmo, int maxAmmo)
+    {
+        ammoText.text = "Ammo: " + curAmmo + " / " + maxAmmo;
+    }
+
+    public void TogglePauseMenu (bool paused)
+    {
+        pauseMenu.SetActive(paused);
+        pauseButton.SetActive(true);
+    }
+
+    public void SetEndGameScreen (bool won, int score)
+    {
+        endGameScreen.SetActive(true);
+        endGameHeaderText.text = won == true ? " You win :D !!" : " You lose :( !!";
+        endGameHeaderText.color = won == true ? Color.green : Color.red;
+        endGameScoreText.text = "<b>Score</b>\n" + score;
+    }
+
+    public void OnContinueButton()
+    {
+
+    }
+
+    public void OnPauseButton()
+    {
+        TogglePauseMenu(true);
+        pauseButton.SetActive(false);
+    }
+
+    public void OnRestartButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnMenuButton()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
